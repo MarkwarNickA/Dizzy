97a2a62fba65ae1140b69913d9aef8255fe2b846
lewis <lewisgarmston@gmail.com>
2016-09-17 14:41:15 -0700

    relative head positioning for avatar

--- DizzyVR/Assets/DizzyVR/Scripts/miniMeObject.cs
+++ DizzyVR/Assets/DizzyVR/Scripts/miniMeObject.cs
@@ -11,11 +11,16 @@ public class miniMeObject : MonoBehaviour
     public Vector3 bodyPartScale;
 
     public GameObject cameraRig;
+
+    public bool AllowAvatarTranslation;
+
     GameObject trackedObject;
     float miniModelScale;
 
     Vector3 miniModelOffset;
     MiniModel myMiniModel;
+
+    GameObject trackedHeadObject;
 
 
     void Start()
@@ -35,6 +40,9 @@ public class miniMeObject : MonoBehaviour
                 break;
         }
 
+        //We need the head to make relative positions from the hands (i.e. fix the head)
+        trackedHeadObject = cameraRig.transform.GetChild(2).gameObject;
+
         bodyPartScale = gameObject.transform.localScale;
         myMiniModel = cameraRig.GetComponent<MiniModel>();
     }
@@ -44,14 +52,42 @@ public class miniMeObject : MonoBehaviour
         miniModelScale = myMiniModel.miniModelScale;
         miniModelOffset = myMiniModel.miniModelOffset;
 
-        miniPosition = trackedObject.transform.position;
-        miniPosition *= miniModelScale;
-        miniPosition += miniModelOffset;
-        miniPosition += cameraRig.transform.position;
 
-        miniRotation = trackedObject.transform.rotation;
 
-        miniScale = bodyPartScale * miniModelScale;
+        //miniPosition = trackedObject.transform.position;
+
+        if (AllowAvatarTranslation == true)
+        {
+            string bodyPartName = gameObject.name;
+            if (bodyPartName != "Head")
+            {
+                //Get the position relative to the head
+                var differenceBetweenHeadAndObject = trackedObject.transform.position - trackedHeadObject.transform.position;
+                miniPosition = cameraRig.transform.position;
+                miniPosition *= miniModelScale;
+                miniPosition += miniModelOffset;
+                miniPosition += differenceBetweenHeadAndObject;
+            }
+            else
+            {
+                // Don't update the position of the head - this sdhould only be updated by the vive wands.  
+                miniPosition = cameraRig.transform.position;
+                miniPosition *= miniModelScale;
+                miniPosition += miniModelOffset;
+            }
+
+            miniRotation = trackedObject.transform.rotation;
+            miniScale = bodyPartScale * miniModelScale;
+        }
+        else {
+            miniPosition = trackedObject.transform.position;
+            miniPosition *= miniModelScale;
+            miniPosition += miniModelOffset;
+            miniPosition += cameraRig.transform.position;
+        }
+
+
+        //Update this hand/head
         gameObject.transform.localScale = miniScale;
         gameObject.transform.position = miniPosition;
         gameObject.transform.rotation = miniRotation;
