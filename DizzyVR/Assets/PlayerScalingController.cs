5ac7b4fdaf2c00c2a23cb115b7c4ab8ed0629efd
Nicholas Abel <nick.a.abel@gmail.com>
2016-09-17 00:44:54 -0700

    Scaling and movement detection stubs

---
+++ DizzyVR/Assets/PlayerScalingController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+using NewtonVR;
+
+namespace Assets
+{
+    class PlayerScalingController : MonoBehaviour
+    {
+        NVRHand Hand;
+
+        bool ScalingOn;
+
+        public void Awake()
+        {
+            Hand = this.GetComponent<NVRHand>();
+        }
+
+        public void Update()
+        {
+            if (Hand && !Hand.IsInteracting)
+            {
+                if (Hand.UseButtonDown)
+                {
+                    ScalingOn = true;
+                }
+                else
+                {
+                    ScalingOn = false;
+                }
+            }
+
+            if (ScalingOn)
+            {
+
+            }
+        }
+
+        private float ModifyScale( float percentage, float maxMultiplier )
+        {
+            return 1f;
+        }
+    }
+}
