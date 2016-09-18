8e0b8e90ebf597d68f277ad807f99a57e383e288
Nicholas Abel <nick.a.abel@gmail.com>
2016-09-17 21:35:05 -0700

    Added fading scripts

---
+++ DizzyVR/Assets/DizzyVR/Scripts/FadeController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeController : MonoBehaviour {
+
+    public int RenderQueue;
+
+	// Use this for initialization
+	void Start () {
+        Renderer LocalRender;
+        LocalRender = this.GetComponent<Renderer>();
+        LocalRender.material.renderQueue = RenderQueue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void Fade(float amount)
+    {
+        this.GetComponent<Renderer>().material.color = new Color(0, 0, 0, amount);
+    }
+}
